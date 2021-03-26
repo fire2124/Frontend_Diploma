dbc4322331d431dba8bcbea0917dcd26925c82d8
fire2124 <filip.reichl@gmail.com>
2021-03-26 13:19:40 +0100

    add pop for markers and zoom control to right bottom

--- src/components/Map/GeoPoints.jsx
+++ src/components/Map/GeoPoints.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
-const url = "../../img/"
-const iconSize = [25, 25]
+const url = "../../img/";
+const iconSize = [25, 25];
 
 const bus = new Icon({
   iconUrl: `${url}/markers/bus.png`,
@@ -30,7 +30,7 @@ const trainStops = new Icon({
   iconSize: iconSize,
 });
 
-const getMapIcon = (type) => {
+const getMapIcon = (type) => { // TODO: different colors 
   switch (type) {
     case "MHD":
       return bus;
@@ -44,39 +44,133 @@ const getMapIcon = (type) => {
       return sadStops;
     case "Stop_Train":
       return trainStops;
-      default:
-        break;
+    default:
+      break;
   }
 };
 // case "Traffic":
 //   return // is line or point // TODO: traffic
 
 export const GeoPoints = ({ data }) => {
-  const keys = Object.keys(data).filter(v => v !== 'traffic')
+  const keys = Object.keys(data).filter((v) => v !== "traffic");
   //console.log(keys)
   const points = keys.reduce((acc, currentVal) => {
-    if( data[currentVal] !== null ) {
-      acc = acc.concat(data[currentVal])
+    if (data[currentVal] !== null) {
+      acc = acc.concat(data[currentVal]);
     }
-    return acc
-  }, [])
+    return acc;
+  }, []);
 
-  
-  return (
-    points.map((obj, i) => {
-      if(obj.geometry !== undefined && obj.properties){
-        console.log(obj.properties.Type)
+  return points.map((obj, i) => {
+    if (obj.geometry !== undefined && obj.properties) {
       //console.log([obj.geometry.coordinates[1], obj.geometry.coordinates[0]], obj)
-        return <Marker
-        key={i}
-        position={[obj.geometry.coordinates[1], obj.geometry.coordinates[0]]}
-        icon={getMapIcon(obj.properties.Type)}
-        >
-          
-          <Popup>{obj.properties}</Popup>
-        </Marker>
-        }
+      switch (obj.properties.Type) {
+        case "MHD":
+          return returnMarker(obj, i);
+        case "SAD":
+          return returnMarker(obj, i);
+        case "Train":
+          return returnMarker(obj, i);
+        case "Stop_Mhd":
+          return returnMarkerStop(obj, i);
+        case "Stop_Sad":
+          return returnMarkerStop(obj, i);
+        case "Stop_Train":
+          return returnMarkerStop(obj, i);
+        default:
+          break;
       }
-    ))
+    }
+  });
+};
+
+function returnMarker(obj, i) {
+  return (
+    <Marker
+      key={i}
+      position={[obj.geometry.coordinates[1], obj.geometry.coordinates[0]]}
+      icon={getMapIcon(obj.properties.Type)}
+    >
+      <Popup>
+        <div>
+          <strong>Názov vozidla:</strong>
+          {` ${obj.properties.ROUTE_NUMBER}`}
+        </div>
+        <div>
+          <strong>Typ vozidla:</strong> {` ${obj.properties.Type}`}
+        </div>
+        <div>
+          <strong>Meškanie:</strong>
+          {` ${obj.properties.DELAY} sekúnd`}
+        </div>
+        {obj.properties.Current_Stop ? (
+          <div>
+            <strong>Aktuálna zastávka:</strong>
+            {` ${obj.properties.Current_Stop}`}
+          </div>
+        ) : null}
+        {/* train */}
+        {obj.properties.From && obj.properties.Type === "Train" ? (
+          <div>
+            <strong>Odkiaľ:</strong>
+            {` ${obj.properties.From}`}
+          </div>
+        ) : null}
+        {obj.properties.To && obj.properties.Type === "Train" ? (
+          <div>
+            <strong>Kam:</strong>
+            {` ${obj.properties.To}`}
+          </div>
+        ) : null}
+        {/* sad */}
+        {obj.properties.From && obj.properties.Type === "SAD" ? (
+          <div>
+            <strong>Odkiaľ:</strong>
+            {` ${obj.properties.From}`}
+          </div>
+        ) : null}
+        {obj.properties.To && obj.properties.Type === "SAD" ? (
+          <div>
+            <strong>Kam:</strong>
+            {` ${obj.properties.To}`}
+          </div>
+        ) : null}
+        {/* Mhd */}
+        {obj.properties.To && obj.properties.Type === "MHD" ? (
+          <div>
+            <strong>Kam:</strong>
+            {` ${obj.properties.To}`}
+          </div>
+        ) : null}
+      </Popup>
+    </Marker>
+  );
 }
 
+function returnMarkerStop(obj, i) {
+  return (
+    <Marker
+      key={i}
+      position={[obj.geometry.coordinates[1], obj.geometry.coordinates[0]]}
+      icon={getMapIcon(obj.properties.Type)}
+    >
+      <Popup>
+        <div>
+          <strong>Typ zastávky:</strong>
+          {` ${obj.properties.Type}`}
+        </div>
+        {obj.properties.Type === "Stop_Train" ? (
+          <div>
+            <strong>Názov zastávky:</strong>
+            {` ${obj.properties.description}`}
+          </div>
+        ) : (
+          <div>
+            <strong>Názov zastávky:</strong>
+            {` ${obj.properties.name}`}
+          </div>
+        )}
+      </Popup>
+    </Marker>
+  );
+}
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { GeoPoints } from "./GeoPoints"
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+} from "react-leaflet";
+import { GeoPoints } from "./GeoPoints";
 import "leaflet/dist/leaflet.css";
 
-
-const Map = ({data}) => {
+const Map = ({ data }) => {
   const position = [48.99, 21.244];
-    return (
-      <MapContainer
-        center={position}
-        zoom={10}
-        style={{ height: "95%", width: "150%" }}
-        className="rounded-tr-lg rounded-tl-lg rounded-bl-lg rounded-br-lg "
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <GeoPoints data={data}/>
-      </MapContainer>
-    );
+  return (
+    <MapContainer
+      center={position}
+      zoom={10}
+      style={{ height: "95%", width: "150%" }}
+      className="rounded-tr-lg rounded-tl-lg rounded-bl-lg rounded-br-lg control"
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeoPoints data={data} />
+      <ZoomControl position="bottomright" />
+    </MapContainer>
+  );
 };
 export default Map;
