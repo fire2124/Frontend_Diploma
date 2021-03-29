22742e4a68128c7710c36d40a1698b4858ae39de
fire2124 <filip.reichl@gmail.com>
2021-03-29 13:56:05 +0200

    adding heatmap to delays

--- src/components/Map/Heatmap.jsx
+++ src/components/Map/Heatmap.jsx
@@ -1,39 +1,53 @@
-import React from "react";
-import { render } from "react-dom";
-import {
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  ZoomControl,
-} from "react-leaflet";
-//import HeatmapLayer from '../src/HeatmapLayer';
-//import { addressPoints } from './realworld.10000.js';
+import React, { useEffect } from 'react';
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet.heat";
 
 const Heatmap = ({ data }) => {
+
   const position = [48.99, 21.244];
-  //console.log(data)
+  let finalData = [];
+  if (data !== undefined) {
+    try {
+      data.map((e) => {
+        let array = [];
+        array.push(e.geometry.coordinates[1]);
+        array.push(e.geometry.coordinates[0]);
+        array.push(e.properties.sum);
+        finalData.push(array);
+      });
+      console.log(finalData);
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    let container = L.DomUtil.get("map");
+    if (container != null) {
+      container._leaflet_id = null;
+    }
+    let map = L.map("map").setView(position, 12);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    let points = finalData
+      ? finalData.map((p) => {
+          return [p[0], p[1]];
+        })
+      : [];
+    L.heatLayer(points, { radius: 30 }).addTo(map);
+  }, []);
+
   return (
-    <MapContainer
-      center={position}
-      zoom={10}
-      style={{ height: "95%", width: "150%" }}
+    <div
+      id="map"
+      scrollWheelZoom="true"
+      dragging="true"
       className="rounded-tr-lg rounded-tl-lg rounded-bl-lg rounded-br-lg control"
-      scrollWheelZoom={true}
-     >
-    {/* //   <HeatmapLayer
-    //         fitBoundsOnLoad
-    //         fitBoundsOnUpdate
-    //         points={data.features}
-    //         longitudeExtractor={m => m[1]}
-    //         latitudeExtractor={m => m[0]}
-    //         intensityExtractor={m => parseFloat(m[2])} />  */}
-       <TileLayer
-        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <ZoomControl position="bottomright" />
-    </MapContainer>
+      style={{ height: "95%", width: "150%" }}
+    ></div>
   );
 };
 
--- src/pages/Delays.jsx
+++ src/pages/Delays.jsx
@@ -2,47 +2,47 @@ import React, { useState } from "react";
 import { SidePanel } from "../components/SidePanel";
 import { DelayForm } from "../components/forms/DelayForm";
 import { Card } from "../components/Card";
-import MapExample from "../components/map/Heatmap";
+import Heatmap from "../components/map/Heatmap";
 import { getData } from "../services/Delays";
-import {TrafficIntervalForm} from "../components/forms/TrafficIntervalForm"
+import { TrafficIntervalForm } from "../components/forms/TrafficIntervalForm";
 
 const Delays = () => {
   const [delay, setDelay] = useState(`delay`);
   const [vehicle, setVehicle] = useState(`MHD`);
-  const [trafficInterval, setTrafficInterval] = useState(`15min`)
-  const [data, setData] = useState({})
+  const [trafficInterval, setTrafficInterval] = useState(`15min`);
+  const [data, setData] = useState({});
 
   const delayFormChange = async (form) => {
-    setVehicle(form.vehicle)
-    setDelay(form.delay)
+    setVehicle(form.vehicle);
+    setDelay(form.delay);
     const res = await getData(form.delay, form.vehicle, trafficInterval);
     setData(res);
-  }
+  };
 
   const trafficIntervalFormChange = async (v) => {
-    setTrafficInterval(v.interval)
+    setTrafficInterval(v.interval);
     const res = await getData(delay, vehicle, v.interval);
     setData(res);
-  }
+  };
 
   return (
     <div className="flex my-20 h-full">
-    <SidePanel
-        //title={"Čo popisuje daná heatmapa?"} sekas mi 
+      <SidePanel
+        //title={"Čo popisuje daná heatmapa?"} sekas mi
         className="pr-2"
       >
         <DelayForm onChange={delayFormChange} />
       </SidePanel>
       <Card className="mr-20 flex flex-grow">
-          <div className="text_name py-5 px-5">
-             Meškania / vytváranie meškaní
-          </div>
+        <div className="text_name py-5 px-5">Meškania / vytváranie meškaní</div>
+        <div className="grid gap-2 h-48 ">
           <div className="map my-2">
-            <MapExample data={data}/>
+          <Heatmap data={data.features} />
           </div>
-          <div>
-            <TrafficIntervalForm onChange={trafficIntervalFormChange}/>
-          </div>  
+          <div className=""> {/*tTop*/}
+            <TrafficIntervalForm onChange={trafficIntervalFormChange} />
+          </div>
+        </div>
       </Card>
     </div>
   );
